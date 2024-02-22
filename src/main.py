8804143b27c2facb0621c96f1a6ae117d93d9a34
Vlad Yastrebov <blakkheart66@gmail.com>
2024-02-22 20:56:21 +0300

    added hash and encrypted data

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
 import secrets
 import string
 import os
 import hashlib
-
+import pytz
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from cryptography.fernet import Fernet
 from typing import Annotated, List, Optional
@@ -10,18 +12,36 @@ from fastapi import FastAPI, HTTPException, status
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel, BeforeValidator, ConfigDict, Field
 
-load_dotenv()
 
-app = FastAPI()
+load_dotenv()
 
 MONGO_URL = 'mongodb://127.0.0.1:27017'
 client = AsyncIOMotorClient(MONGO_URL)
 db = client['mydatebase']
 collection = db['items']
-
+# db.items.create_index({"created_at": 1}, {expireAfterSeconds: 86400})
+# db.items.create_index('created_at', expireAfterSeconds=30)
 KEY = os.getenv('KEY', '1234').encode()
 fernet = Fernet(KEY)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await collection.create_index(
+        'expire_at',
+        expireAfterSeconds=0,
+        #     partialFilterExpression={
+        #         'to_delete': True}
+    )
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 PyObjectId = Annotated[str, BeforeValidator(str)]
+
+
+#
 
 
 class Secret(BaseModel):
@@ -30,6 +50,7 @@ class Secret(BaseModel):
     code_phrase: str
     secret_key: str
     salt: str
+    expire_at: datetime = Field(default=None)
     model_config = ConfigDict(
         populate_by_name=True,
         arbitrary_types_allowed=True
@@ -67,6 +88,7 @@ async def generete_secret(secret: ReadSecret) -> str:
         secret=secret_str_bytes.decode('utf-8'),
         code_phrase=code_phrase_bytes.decode('ISO-8859-1'),
         salt=salt.decode('ISO-8859-1'),
+        expire_at=datetime.now(tz=pytz.utc) + timedelta(minutes=5)
     )
     await collection.insert_one(
         db_secret.model_dump(
@@ -91,7 +113,14 @@ async def get_secret(code_phrase: ReadCodePhrase, secret_key: str) -> str:
             secret.get('salt').encode('ISO-8859-1'),
             10000
         ) == secret.get('code_phrase').encode('ISO-8859-1'):
-            secret_decoded = Fernet(KEY).decrypt(secret.get('secret')).decode()
+            secret_decoded = fernet.decrypt(secret.get('secret')).decode()
             return secret_decoded
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.get('/secrets/', response_model_by_alias=True)
+async def get_secrets() -> List[Secret]:
+    secrets = await collection.find().to_list(10000)
+    # print(secrets)
+    return secrets
